3dc97a16c7521b641f9a2432827e0833592f8212
vmt <vuongminhthuy97@gmail.com>
2024-08-24 11:26:41 +0700

    Multiple browsers w Enum

--- IdeaProjects/selenium course/hybrid-framework-nopcommerce/actions/commons/BaseTest.java
+++ IdeaProjects/selenium course/hybrid-framework-nopcommerce/actions/commons/BaseTest.java
@@ -1,5 +1,7 @@
 package commons;
 
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -13,21 +15,31 @@ public class BaseTest {
         private WebDriver driver;
         private String projectPath = System.getProperty("user.dir");
 
-        if (browserName.equalsIgnoreCase("firefox")) {
-            System.setProperty("webdriver.firefox.driver", projectPath + "/browserDrivers/geckodriver.exe");
-            driver = new FirefoxDriver();
-        } else if (browserName.equalsIgnoreCase("chrome")){
-            System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver.exe");
-            driver = new ChromeDriver();
-        } else if (browserName.equalsIgnoreCase("edge")){
-            System.setProperty("webdriver.edge.driver", projectPath + "/browserDrivers/msedgedriver.exe");
-            driver = new EdgeDriver();
-        } else if (browserName.equalsIgnoreCase("safari")){
-            System.setProperty("webdriver.safari.driver", projectPath + "/browserDrivers/safaridriver.exe");
-            driver = new SafariDriver();
-        } else {
-            throw new RuntimeException("Browser name is not valid");
+        BrowserList browser = BrowserList.valueOf(browserName.toUpperCase());
+
+        switch (browser){
+            case FIREFOX:
+                System.setProperty("webdriver.firefox.driver", projectPath + "/browserDrivers/geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+            case CHROME:
+                System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+            case EDGE:
+                System.setProperty("webdriver.edge.driver", projectPath + "/browserDrivers/msedgedriver.exe");
+                driver = new EdgeDriver();
+                break;
+            case SAFARI:
+                System.setProperty("webdriver.safari.driver", projectPath + "/browserDrivers/safaridriver.exe");
+                driver = new SafariDriver();
+                break;
+            default:
+                throw new RuntimeException("Brow ser name is not valid");
         }
+
+        driver.manage().window().setPosition(new Point(0,0));
+        driver.manage().window().setSize(new Dimension(1920,1080));
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.get("https://demo.nopcommerce.com/");
         return driver;
